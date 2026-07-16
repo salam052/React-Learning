85e10db0cec5497e2cfd1c4d3cc543196c8fa1cf
Shahnawaz Alam <shahnawaz052@gmail.com>
2026-07-16 16:10:29 +0300

    Backroad-app project completed

--- backroad-app/src/App.js
+++ backroad-app/src/App.js
@@ -1,8 +1,21 @@
-
+import React from 'react';
+import About from './components/About';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+import Tours from './components/Tours';
 
 function App() {
   return (
-    <h1>Welcome to Backroads</h1>
+    <React.Fragment>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </React.Fragment>
   );
 }
 
